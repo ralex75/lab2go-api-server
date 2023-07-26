837a171889e96f443066e42bfe0508baeb156399
Alessandro Ruggieri <alessandro.ruggieri@roma1.infn.it>
2023-07-26 11:58:18 +0200

    fix

--- api/global.js
+++ api/global.js
@@ -2,11 +2,12 @@ const LAB2GO_MAIL="info.lab2go@gmail.com"
 const MASTER_MAIL="francesco.safaitehrani@roma1.infn.it"
 const NO_REPLY="noreply@infn.it"
 const mailext={"REQSUB":"reqsub","REQACC":"reqacc"}
+const LAB2GO_BASE_URL="https://lab2go-apps.roma1.infn.it/lab2go"
 
 //BASE URL APP PROD
 const LAB2GO_URL={
-    REQUESTS:{"PROD":"https://lab2go-apps.roma1.infn.it/lab2go/richieste/","DEV":"localhost:3000/"},
-    ADMIN:{"PROD":"https://lab2go-apps.roma1.infn.it/lab2go/admin/","DEV":"localhost:3000/"}
+    REQUESTS:{"PROD":`${LAB2GO_BASE_URL}/richieste/`,"DEV":"localhost:3000/"},
+    ADMIN:{"PROD":`${LAB2GO_BASE_URL}/admin/`,"DEV":"localhost:3000/"}
 }
 
 const mail={
